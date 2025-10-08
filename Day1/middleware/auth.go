4530ae61c967e3e71df62492134e287f320d03ca
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2025-10-09 00:59:36 +0500

    Uses Docker to Deploy my API with JWT TOken for Secure API

--- Day1/middleware/auth.go
+++ Day1/middleware/auth.go
@@ -1,1 +1,52 @@
-package middeleware
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("my_secret_key") // Change this to something strong
+
+// GenerateJWT generates a token when user logs in
+func GenerateJWT(username string) (string, error) {
+	claims := jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(), // expires in 24h
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(jwtKey)
+}
+
+// ValidateJWT middleware to check token
+func ValidateJWT(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Missing Authorization Header", http.StatusUnauthorized)
+			return
+		}
+
+		// Format: "Bearer <token>"
+		tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method")
+			}
+			return jwtKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid Token", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
--- Day1/routes/userRoutes.go
+++ Day1/routes/userRoutes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"Day1/controllers"
+	"Day1/middleware"
 
 	"github.com/gorilla/mux"
 )
@@ -10,13 +11,25 @@ func SetupRoute() *mux.Router {
 
 	routes := mux.NewRouter() // create routes for the endpoint of api
 
-	routes.HandleFunc("/tasks", controllers.CreateTask).Methods("POST")      // post and create tasks
-	routes.HandleFunc("/tasks", controllers.GetTasks).Methods("GET")         // Get all the tasks
-	routes.HandleFunc("/tasks/{id}", controllers.GetTaskByID).Methods("GET") // Get Specifc Id task
+	routes.HandleFunc("/login", controllers.Login).Methods("POST") // Public Route anyone can login and can get the jwt token 
 
-	routes.HandleFunc("/tasks/{id}", controllers.UpdateTask).Methods("PUT") // update specifc task
+	api := routes.PathPrefix("/api").Subrouter() // all other routes required the jwt token and all are protected
 
-	routes.HandleFunc("/tasks/{id}", controllers.DeleteTask).Methods("DELETE") // delete a task
+
+	// you can use subrouter inside the routes but the upper one is resusable by pathprefix
+
+	// which is like api/tasks now become this url
+
+	api.Use(middleware.ValidateJWT) // use jwt for validation // with this line all routes are protected
+	
+
+	// now all the routes enpoint becomes like api/tasks , api/tasks{id} ...
+
+	api.HandleFunc("/tasks", controllers.CreateTask).Methods("POST")        // post and create tasks
+	api.HandleFunc("/tasks", controllers.GetTasks).Methods("GET")           // Get all the tasks
+	api.HandleFunc("/tasks/{id}", controllers.GetTaskByID).Methods("GET")   // Get Specifc Id task
+	api.HandleFunc("/tasks/{id}", controllers.UpdateTask).Methods("PUT")    // update specifc task
+	api.HandleFunc("/tasks/{id}", controllers.DeleteTask).Methods("DELETE") // delete a task
 
 	return routes // return all routes
 }
